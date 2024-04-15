411bb6efae0588fc3734efec9f2fd666d38635e5
rertyy <nxtms3@gmail.com>
2024-04-15 23:22:37 +0800

    Fix negative 0 bug

--- src/main/java/seedu/address/model/order/Price.java
+++ src/main/java/seedu/address/model/order/Price.java
@@ -50,7 +50,7 @@ public class Price {
 
     @Override
     public String toString() {
-        return String.format("%.2f", value);
+        return String.format("%.2f", value + 0.0);
     }
 
     @Override
